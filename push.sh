7685a154f4e216ec4131f1a5fee6c03bdda0f830
ondm <94042519+ondm@users.noreply.github.com>
2021-12-15 21:43:36 +0800

    Update push.sh

--- push.sh
+++ push.sh
@@ -4,9 +4,9 @@ while true; do
 MAXTEMP=$({(nvidia-smi -q | grep "GPU Current Temp" | awk '{print $5}')} | \
  awk '{if(max=="") {max=$1}; if($1>max) {max=$1}} END {print max}')
 if [ $MAXTEMP -lt "30" ] ; then     
-    curl -s "https://push.dnson.cn/?title=Linux101&des=$MAXTEMP"
+    curl -s "https://xxx.cn/?title=Linux101&des=$MAXTEMP"
 elif [ $MAXTEMP -gt "70" ] ; then   
-    curl -s "https://push.dnson.cn/?title=Linux101&des=$MAXTEMP" 
+    curl -s "https://xxx.cn/?title=Linux101&des=$MAXTEMP" 
 else 
     exit 0 
 fi    
